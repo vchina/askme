01b3500dc9d301fdd24f275c6dd7d1966ef56007
Dmitry Vcina <dmitriy@vchina.ru>
2023-04-24 23:42:28 +0300

    partials, helpers, flash

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,28 +1,26 @@
 class QuestionsController < ApplicationController
-  # skip_before_action :verify_authenticity_token
   before_action :set_question, only: %i[show edit update destroy hide unhide]
   def create
     question = Question.create(question_params)
-    # debugger
-    redirect_to question_path(question)
+    redirect_to question_path(question), notice: 'Новый вопрос создан'
   end
 
   def update
     @question.update(question_params)
-
-    redirect_to question_path(@question)
+    redirect_to question_path(@question), notice: 'Вопрос обновлён'
   end
 
   def destroy
     @question.destroy
-    redirect_to questions_path
+    redirect_to questions_path, notice: 'Вопрос удалён'
   end
 
   def show
   end
 
   def index
-    @question = Question.all
+    @questions = Question.all
+    @question = Question.new
   end
 
   def new
@@ -34,12 +32,12 @@ class QuestionsController < ApplicationController
 
   def hide
     @question.update(is_hidden: true)
-    redirect_to questions_path
+    redirect_to question_path, notice: 'Вопрос скрыт'
   end
 
   def unhide
     @question.update(is_hidden: false)
-    redirect_to questions_path
+    redirect_to question_path, notice: 'Вопрос отображен'
   end
 
   private
