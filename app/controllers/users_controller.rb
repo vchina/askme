6ed5f8cc7877f07ffb2023ac9f8eabc0e6b2cce8
Dmitry Vcina <dmitriy@vchina.ru>
2023-04-27 00:57:59 +0300

    Autologin after registration in askme

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
   def create
     user_params = params.require(:user).permit(:name, :nickname, :email, :password)
     @user = User.create(user_params)
-
+    session[:user_id] = @user.id
+    
     redirect_to questions_path, notice: 'Вы успешно зарегистрировались'
   end
 
   def new
-    debugger
     @user = User.new
   end
 end
