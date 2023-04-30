0cf8cf636b97767dca2b0dc044d26977b8360592
Dmitry Vcina <dmitriy@vchina.ru>
2023-04-30 11:57:06 +0300

    add partials to _questions.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-
   root to: 'questions#index'
 
-  resources :questions
-  put '/questions/:id/hide', to: 'questions#hide'
-  put '/questions/:id/unhide', to: 'questions#unhide'
+  resources :questions do
+    member do
+      put :hide
+      put :unhide
+    end
+  end
 
   resource :session, only: %i[new create destroy]
   resources :users, only: %i[new create]
