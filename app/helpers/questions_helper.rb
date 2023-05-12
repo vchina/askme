d89b082f817a6023c99449dbdc488c07d85df9af
Dmitriy Vchina <dmitriy@vchina.ru>
2023-05-12 14:07:39 +0300

    Custom errors page

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,7 +2,7 @@ module QuestionsHelper
   def show_author(question)
     if question.author_id.present?
       link_to"@#{question.author.nickname}", user_path(question.author.nickname)
-    else question.author == nil
+    else
       'Автор вопроса неизвестен'
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,9 @@ Rails.application.routes.draw do
 
   resource :session, only: %i[new create destroy]
   resources :users, param: :nickname, except: %i[index]
+
+  match '/404', via: :all, to: 'errors#not_found'
+  match '/422', via: :all, to: 'errors#unprocessable_entity'
+  match '/500', via: :all, to: 'errors#server_error'
+  
 end
